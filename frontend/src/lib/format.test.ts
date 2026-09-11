19da06931182dbdbd160c5639d605ca498bc48de
Kubilay Caglayan <contactkubilay@gmail.com>
2026-09-11 14:20:32 +0000

    fix(ui): preserve minutes in long durations

--- frontend/src/lib/format.test.ts
+++ frontend/src/lib/format.test.ts
@@ -15,8 +15,10 @@ describe("formatTrackedDuration", () => {
     [4980, "1h 23 minutes"],
     [7200, "2h"],
     [24 * 60 * 60, "24h"],
-    [24 * 60 * 60 + 60, "24h"],
-    [25 * 60 * 60 + 23 * 60, "25h"],
+    [24 * 60 * 60 + 60, "24h 1 minute"],
+    [25 * 60 * 60 + 23 * 60, "25h 23 minutes"],
+    [99 * 60 * 60 + 59 * 60, "99h 59 minutes"],
+    [100 * 60 * 60 + 59 * 60, "100h"],
     [-1, "0 seconds"],
     [59.9, "59 seconds"],
   ])("formats %s seconds as %s", (seconds, expected) => {
