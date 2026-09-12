4be3152e77b982e392d61cc68564a667daa88bd9
Kubilay Caglayan <contactkubilay@gmail.com>
2026-09-12 15:10:06 +0000

    Move legacy highlight cleanup to new migration

--- backend/src/main/resources/db/migration/V37__remove_highlight_system_status.sql
+++ backend/src/main/resources/db/migration/V37__remove_highlight_system_status.sql
@@ -1,8 +1,3 @@
-delete from log_label
-where label_id in (
-    select id from labels
-    where system = true and lower(name) = 'highlight'
-);
-
-delete from labels
-where system = true and lower(name) = 'highlight';
+update labels
+set system = false
+where lower(name) = 'highlight';
