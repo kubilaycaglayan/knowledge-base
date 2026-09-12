48cb143b6be98b03ee207be42a2d3329297c173a
Kubilay Caglayan <contactkubilay@gmail.com>
2026-09-12 15:02:08 +0000

    Remove seeded highlight labels

--- backend/src/main/resources/db/migration/V37__remove_highlight_system_status.sql
+++ backend/src/main/resources/db/migration/V37__remove_highlight_system_status.sql
@@ -1,3 +1,8 @@
-update labels
-set system = false
-where lower(name) = 'highlight';
+delete from log_label
+where label_id in (
+    select id from labels
+    where system = true and lower(name) = 'highlight'
+);
+
+delete from labels
+where system = true and lower(name) = 'highlight';
